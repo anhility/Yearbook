57fb1c933202ba044afde4ac013d47b9a864ac90
anhility <anhility@gmail.com>
2017-11-27 06:37:01 +0100

    Preperation for future localization

---
+++ Locales/enUS.lua
@@ -0,0 +1,4 @@
+local L = LibStub("AceLocale-3.0"):NewLocale("Yearbook", "enUS", true)
+
+--Example
+--L["Patch %s"] = true
---
+++ Locales/itIT.lua
@@ -0,0 +1,4 @@
+local L = LibStub("AceLocale-3.0"):NewLocale("Yearbook", "itIT")
+if L then
+--@localization(locale="itIT", format="lua_additive_table", escape-non-ascii="true", handle-unlocalized="ignore")@
+end
